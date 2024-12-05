65929e5d4be16d4c7e04168711999bd38d6e39f2
Leonardo Richard <leonardo.richard@pm.pe.gov.br>
2024-12-05 12:48:14 -0300

    chore: created initial project structure

---
+++ src/main/java/br/com/loomi/interview/InterviewloomiApplication.java
@@ -0,0 +1,13 @@
+package br.com.loomi.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewloomiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewloomiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/loomi/interview/InterviewloomiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.loomi.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewloomiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
